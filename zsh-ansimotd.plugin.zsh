e099ae89b71bdd3978ba14506bb12e94ecb32548
Clint Plummer <clint@yuhonas.com>
2021-11-21 14:55:43 +1000

    small cleanups

--- zsh-ansimotd.plugin.zsh
+++ zsh-ansimotd.plugin.zsh
@@ -22,14 +22,18 @@ function ansi_art_random_file {
   unzip -Z1 "$1"  | grep -i -E ".*\.(ans|asc)$" | shuf -n 1
 }
 
-zip_filename=$(ansi_art_random_zip)
-ansi_filename=$(ansi_art_random_file $zip_filename)
+function ansi_art_random {
+  local zip_filename=$(ansi_art_random_zip)
+  local ansi_filename=$(ansi_art_random_file $zip_filename)
 
-if [ $? -eq 0 ]; then
-  # print and decode to stdout
-  unzip -q -p "$zip_filename" "$ansi_filename" | iconv -f 437
+  if [ -n "$ansi_filename" ]; then
+    # print and decode to stdout
+    unzip -q -p "$zip_filename" "$ansi_filename" | iconv -f 437
 
-  # save these incase the user wants to find them later
-  export ANSI_MOTD_ZIP="$zip_filename"
-  export ANSI_MOTD_FILENAME="$ansi_filename"
-fi;
+    # save these incase the user wants to find them later
+    export ANSI_MOTD_ZIP="$zip_filename"
+    export ANSI_MOTD_FILENAME="$ansi_filename"
+  fi;
+}
+
+ansi_art_random
